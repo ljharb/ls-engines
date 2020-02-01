7b432ce9c908b0612e5b90052fb3c8d2daac5f0c
Jordan Harband <ljharb@gmail.com>
2020-02-01 13:55:13 -0800

    [Fix] "virtual" only works properly with lockfileVersion >= 2
     - thus, we have to, for each package in the graph, ask pacote for a full manifest :-(

--- get-tree.js
+++ get-tree.js
@@ -2,13 +2,14 @@
 
 const Arborist = require('@npmcli/arborist');
 const chalk = require('chalk');
+const pacote = require('pacote');
 
 const arb = new Arborist();
 
 const npmInfo = require('./npm-info');
 
 module.exports = async function getTree(mode, logger = (x) => console.log(x)) {
-	const { hasNodeModules, hasLockfile, hasPackage } = await npmInfo(mode);
+	const { hasNodeModules, hasLockfile, hasPackage, lockfileVersion } = await npmInfo(mode);
 
 	if (mode === 'actual' || hasNodeModules) {
 		const messages = [].concat(
@@ -20,12 +21,28 @@ module.exports = async function getTree(mode, logger = (x) => console.log(x)) {
 	}
 
 	if (mode === 'virtual' || hasLockfile) {
+		if (hasLockfile && lockfileVersion < 2) {
+			const messages = [].concat(
+				hasLockfile ? 'v1 lockfile found' : [],
+				mode === 'virtual' ? 'mode is “virtual”' : [],
+			);
+			logger(chalk.green(`${messages.join(', ')}; loading ideal tree from lockfile...`));
+			const tree = await arb.buildIdealTree({ fullMetadata: true });
+			await Promise.all(Array.from(
+				tree.children.values(),
+				async (node) => {
+					// eslint-disable-next-line no-param-reassign
+					node.package = await pacote.manifest(node.name, { fullMetadata: true });
+				},
+			));
+			return tree;
+		}
 		const messages = [].concat(
 			hasLockfile ? 'Lockfile found' : [],
 			mode === 'virtual' ? 'mode is “virtual”' : [],
 		);
 		logger(chalk.green(`${messages.join(', ')}; loading virtual tree from lockfile...`));
-		return arb.loadVirtual();
+		return arb.loadVirtual({ fullMetadata: true });
 	}
 
 	const messages = [].concat(
@@ -33,5 +50,5 @@ module.exports = async function getTree(mode, logger = (x) => console.log(x)) {
 		mode === 'ideal' ? 'mode is “ideal”' : [],
 	);
 	logger(chalk.green(`${messages.join(', ')}; building ideal tree from \`${chalk.gray('package.json')}\`...`));
-	return arb.buildIdealTree();
+	return arb.buildIdealTree({ fullMetadata: true, update: { all: true } });
 };
--- npm-info.js
+++ npm-info.js
@@ -8,27 +8,57 @@ module.exports = async function npmInfo(mode) {
 		? fs.stat(path.join(process.cwd(), 'node_modules')).catch(() => false)
 		: false;
 
+	const packagePath = path.join(process.cwd(), 'package.json');
+	const packageLockPath = path.join(process.cwd(), 'package-lock.json');
+	const shrinkwrapPath = path.join(process.cwd(), 'npm-shrinkwrap.json');
+	const pHasPackageLock = fs.stat(packageLockPath).then(() => true, () => false);
+
 	const pHasLockfile = mode === 'auto' || mode === 'virtual'
-		? fs.stat(path.join(process.cwd(), 'package-lock.json')).catch(() => fs.stat(path.join(process.cwd(), 'npm-shrinkwrap.json'))).catch(() => false)
+		? pHasPackageLock.catch(async (hasPackageLock) => {
+			if (!hasPackageLock) {
+				return false;
+			}
+			return fs.stat(shrinkwrapPath).then(() => true, () => false);
+		})
 		: false;
 
 	const pHasPackage = mode === 'auto' || mode === 'ideal'
-		? fs.stat(path.join(process.cwd(), 'package.json')).catch(() => false)
+		? fs.stat(packagePath).catch(() => false)
 		: false;
+
+	/* eslint-disable consistent-return, global-require */
+	const pLockfileVersion = Promise.all([
+		pHasLockfile,
+		pHasPackageLock,
+	]).then(async ([
+		hasLockfile,
+		hasPackageLock,
+	]) => {
+		if (hasPackageLock) {
+			return require(packageLockPath).lockfileVersion;
+		}
+		if (hasLockfile) {
+			return require(shrinkwrapPath).lockfileVersion;
+		}
+	});
+	/* eslint-enable consistent-return, global-require */
 
 	const [
 		hasLockfile,
 		hasNodeModules,
 		hasPackage,
+		lockfileVersion,
 	] = await Promise.all([
 		pHasLockfile,
 		pHasNodeModules,
 		pHasPackage,
+		pLockfileVersion,
 	]);
 
 	return {
 		hasLockfile,
 		hasNodeModules,
 		hasPackage,
+		lockfileVersion,
 	};
 };
