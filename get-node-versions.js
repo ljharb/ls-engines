5f15ebce73463cabe3e5043de6bc246836597618
Jordan Harband <ljharb@gmail.com>
2020-02-24 00:01:02 -0800

    [Breaking] `get-node-versions`: don’t bother with node < 0.4

--- get-node-versions.js
+++ get-node-versions.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const getJSON = require('get-json');
+const semver = require('semver');
 
 module.exports = async function getNodeVersions() {
 	const index = await getJSON('https://nodejs.org/dist/index.json').catch((e) => {
 		console.error('Error fetching and parsing JSON from `https://nodejs.org/dist/index.json`');
 		throw e;
 	});
-	return index.map(({ version }) => version);
+	return index
+		.map(({ version }) => version)
+		.filter((version) => semver.satisfies(version, '>= 0.4')); // no need to consider older than 0.4
 };
