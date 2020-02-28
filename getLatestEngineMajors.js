4a1b9b2f38077f4d0cd43316b55b28fcdf27563c
Jordan Harband <ljharb@gmail.com>
2020-02-27 23:11:38 -0800

    [Refactor] extract `getLatestEngineMajors`

---
+++ getLatestEngineMajors.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fromEntries = require('object.fromentries');
+
+const getLatestMajors = require('./getLatestMajors');
+
+module.exports = async function getLatestEngineMajors(
+	selectedEngines,
+	allVersions,
+	rootRanges,
+	graphRanges,
+) {
+	const majorEntries = await Promise.all(selectedEngines.map(async (engine) => {
+		const { validRange: rootRange } = rootRanges[engine];
+		const { validRange: graphRange } = graphRanges[engine];
+		const versions = allVersions[engine];
+		const rootMajors = getLatestMajors(versions, rootRange);
+		const graphMajors = getLatestMajors(versions, graphRange);
+		return [engine, { graph: graphMajors, root: rootMajors }];
+	}));
+	return fromEntries(majorEntries);
+};
