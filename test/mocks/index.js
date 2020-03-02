42e74561fc809988e89ae9eedd3299acab764d7e
Jordan Harband <ljharb@gmail.com>
2020-03-02 09:16:02 -0800

    [Tests] mock out network requests

---
+++ test/mocks/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Module = require('module');
+
+const getJSON = require('get-json');
+
+const id = require.resolve('get-json');
+const mod = new Module(id);
+mod.exports = (url, ...args) => {
+	if (url === 'http://nodejs.org/dist/index.json') {
+		return require('./node-versions.json'); // eslint-disable-line global-require
+	}
+	return getJSON(url, ...args);
+};
+require.cache[id] = mod;
+
+process.env.NODE_OPTIONS = `--require="${require.resolve('.')}"`;
