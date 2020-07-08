94624d7516e63c2885ff720900f9b54e38184cb8
Jordan Harband <ljharb@gmail.com>
2020-07-08 12:54:50 -0700

    [Fix] `getNodeVersions`: use HTTPS instead of HTTP
     - also, add debug info with the URL
    
    Fixes #21.

--- get-node-versions.js
+++ get-node-versions.js
@@ -3,6 +3,9 @@
 const getJSON = require('get-json');
 
 module.exports = async function getNodeVersions() {
-	const index = await getJSON('http://nodejs.org/dist/index.json');
+	const index = await getJSON('https://nodejs.org/dist/index.json').catch((e) => {
+		console.error('Error fetching and parsing JSON from `https://nodejs.org/dist/index.json`');
+		throw e;
+	});
 	return index.map(({ version }) => version);
 };
